b92dbf620371ba6b993bb6c1a575a192d1c1445f
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2024-04-24 12:45:56 +0530

    React Basic Project Practices

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      {/** 
+                      Project -1  
+       Build an EmployeeCard component in React to display name,designation and work experience of a person. Pass name, designation and work experience as props.
+          1. The colour of “Designation:” should be green.
+          2. The colour of “Experience:” should be blue.
+      */}
+
+      {/** Project -2  */}
+      {/** Project -3  */}
+      {/** Project -4  */}
+      {/** Project -5  */}
+      {/** Project -6  */}
+    </>
+  );
+};
+
+export default App;
