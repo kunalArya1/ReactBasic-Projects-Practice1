9aead592ea024ed808395d6628d9d20427eaf0f8
kunalArya1 <kunalkrraj220@gmail.com>
2024-05-27 17:03:22 +0530

    Project 9 Completed

---
+++ Project_9/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import EmployeDetails from "./Component/EmployeDetails";
+
+const App = () => {
+  return (
+    <div>
+      <EmployeDetails />
+    </div>
+  );
+};
+
+export default App;
